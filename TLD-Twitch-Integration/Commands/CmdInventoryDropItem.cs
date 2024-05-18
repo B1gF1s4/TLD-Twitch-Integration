4edb92ff0197002af0431b0f88888c97264f2f9c
B1gF1s4 <120759854+B1gF1s4@users.noreply.github.com>
2024-05-18 19:37:44 +0200

    dirty fix for water roll on drop random item redeem

--- TLD-Twitch-Integration/Commands/CmdInventoryDropItem.cs
+++ TLD-Twitch-Integration/Commands/CmdInventoryDropItem.cs
@@ -1,4 +1,5 @@
-﻿using TLD_Twitch_Integration.Exceptions;
+﻿using Il2Cpp;
+using TLD_Twitch_Integration.Exceptions;
 using TLD_Twitch_Integration.Game;
 using TLD_Twitch_Integration.Twitch.Models;
 
@@ -7,7 +8,8 @@ namespace TLD_Twitch_Integration.Commands
 	public class CmdInventoryDropItem : CommandBase
 	{
 
-		private readonly Random random = new();
+		private readonly Random _random = new();
+		private int _dropAttempts = 0;
 
 		public CmdInventoryDropItem() : base("tti_inventory_random")
 		{ }
@@ -25,12 +27,9 @@ namespace TLD_Twitch_Integration.Commands
 				throw new RequiresRedeemRefundException(
 					"Player has no items in inventory.");
 
-			var gearItem =
-				GameService.GearItems[random.Next(0, GameService.GearItems.Count - 1)];
+			var droppedItem = TryDropItem();
 
 			GameService.PlayPlayerSound("PLAY_FEARAFFLICTION");
-
-			var droppedItem = gearItem.Drop(1, true, false, true);
 
 			if (!Settings.ModSettings.AllowItemPickup)
 				droppedItem.enabled = false;
@@ -40,9 +39,36 @@ namespace TLD_Twitch_Integration.Commands
 				alert = $"random item dropped";
 			else
 				alert = $"{redeem.UserName} redeemed '{redeem.CustomReward?.Title}' " +
-					$"-> {gearItem.name}";
+					$"-> {droppedItem.DisplayName}";
 
 			return alert;
+		}
+
+		private GearItem TryDropItem()
+		{
+			if (_dropAttempts >= 5)
+				throw new RequiresRedeemRefundException(
+					"Cant drop item from inventory.");
+
+			_dropAttempts++;
+
+			var gearItem = GameService.GearItems[_random.Next(
+				0, GameService.GearItems.Count - 1)];
+
+			if (gearItem.name == "GEAR_WaterSupplyPotable" ||
+				gearItem.name == "GEAR_WaterSupplyNotPotable")
+				return TryDropItem();
+
+			GearItem droppedItem;
+
+			if (gearItem.m_StackableItem != null)
+				droppedItem = gearItem.Drop(
+					gearItem.m_StackableItem.m_Units, true, false, true);
+
+			else
+				droppedItem = gearItem.Drop(1, true, false, true);
+
+			return droppedItem;
 		}
 	}
 }
